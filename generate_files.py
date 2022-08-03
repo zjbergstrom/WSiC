62f14e692cd2727e5a190c0c7d9a4aef7679eae9
Zack <zjbergstrom@gmail.com>
2022-08-03 12:04:21 -0700

    cleaned up

--- generate_files.py
+++ generate_files.py
@@ -119,7 +119,7 @@ def generateInputs(structures):
 if __name__ == "__main__":
     generateInputs(getStructures(filename="filenames0.txt",dir="structurefiles/"))
     generateInputs(getStructures(filename="filenames1.txt",dir="structurefiles/"))
-    generateInputs(getStructures(filename="filenames2.txt",dir="ternary_reference_compounds/"))
+    generateInputs(getStructures(filename="filenames2.txt",dir="ternary_reference_compounds_structures/"))
     df = pd.DataFrame(data)
     print(df)
     df.to_pickle('structure_data.pkl')
